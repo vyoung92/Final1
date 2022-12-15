0acb6ccbb7786d9adba5afd9fa9a68cd6cb36028
vyoung92 <120543966+vyoung92@users.noreply.github.com>
2022-12-14 20:46:57 -0800

    Update Vincent_Young_Final.py

--- Vincent_Young_Final.py
+++ Vincent_Young_Final.py
@@ -15,7 +15,7 @@
 #Q1 Read in the data, call the dataframe "s"  and check the dimensions of the dataframe
 import pandas as pd
 import streamlit as st
-!pip install scikit-learn as sklearn
+pip import scikit-learn as sklearn
 import scikit-learn as sklearn
 s = pd.read_csv("social_media_usage.csv")
 print(s)
